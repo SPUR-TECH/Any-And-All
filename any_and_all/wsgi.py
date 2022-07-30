902fb1926ae7e13de2eb42c97abc50c5e86d329a
Scott Atkinson <scott.atkin79@gmail.com>
2022-07-30 09:21:43 +0000

    initial commit and set up allauth

---
+++ any_and_all/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for any_and_all project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'any_and_all.settings')
+
+application = get_wsgi_application()
