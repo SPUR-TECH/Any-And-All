afa1b6385c69abca8f68b388ebf5ae553048a02f
Scott Atkinson <scott.atkin79@gmail.com>
2022-07-31 14:37:39 +0000

    added product detail template and view

--- products/views.py
+++ products/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+
 #  view from code institute Boutique Ado
-
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -14,3 +13,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+#  view from code institute Boutique Ado
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
