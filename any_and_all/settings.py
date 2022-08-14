bad9fe66f5c2765f698c40742f1caeae6a9d4dcb
Scott Atkinson <scott.atkin79@gmail.com>
2022-08-14 12:03:56 +0000

    cerate checkout app, template, model, views, signals from boutique ado

--- any_and_all/settings.py
+++ any_and_all/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+     # Other
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -70,6 +73,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'any_and_all.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -84,8 +89,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
