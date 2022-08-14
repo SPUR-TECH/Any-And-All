a4d5cd1a0a622271d8228721258ca23ee627fdaa
Scott Atkinson <scott.atkin79@gmail.com>
2022-08-14 12:45:38 +0000

    set up stripe checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
+# checkout view from code institute Boutique Ado
+
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -14,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LK18YLL987qhGQ332bSzdDbUgVQb4hcfZgVV4Z7uxqneNaF4JDh5ebVS7Zj3UAgQ9c0mNfNnyXDtX9NRxuJ7OiV00x5iIA8Uu',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
