47d6dbf9f15d30c2962c1d34dc46ea450bd49d01
Scott Atkinson <scott.atkin79@gmail.com>
2022-08-27 13:59:54 +0000

    changed procfile

--- any_and_all/settings.py
+++ any_and_all/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['any-and-all.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['any-and-all.herokuapp.com/', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-spurtech-anyandall-f8glbtrh0e2.ws-eu63.gitpod.io']
 
