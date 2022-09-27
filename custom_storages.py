70f34362cacf603fb7af215c6b19f489d3974ced
Scott Atkinson <scott.atkin79@gmail.com>
2022-09-27 19:23:26 +0000

    add doc strings to custon storages.py file, restyle error pages messages and clear readme.md file

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,21 @@
+# Custom storage from code institute Boutique Ado -->
+
+'''
+Imports relevant django packages
+'''
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Directs Django to static files configuration in settings.py
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Directs Django to media files configuration in settings.py
+    """
     location = settings.MEDIAFILES_LOCATION
